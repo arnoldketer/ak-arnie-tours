c980c373e13f094501d77bdea6bac77e47f48b49
Arnold <dioufarnold02@gmail.com>
2024-02-17 00:22:37 +0300

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 
 import './App.css'
 import Navbar from './components/Navbar';
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Contact from './routes/Contact';
+import Service from './routes/Service';
 
 function App() {
   return (
     <div className='App'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/service' element={<Service/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
         <Navbar/>
+        
     </div>
 
   );
