d7211bdd427ec1ddb00d027afd47c6967f09a786
Arnold <dioufarnold02@gmail.com>
2024-02-18 17:10:56 +0300

    Added AboutUs  Component

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -2,6 +2,7 @@ import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import AboutImg from "../assets/night.jpg";
 import Footer from "../components/Footer";
+import AboutUs from "../components/AboutUs";
 
 function About () {
     return(
@@ -13,6 +14,7 @@ function About () {
                 title="About"
                 btnClass="hide"
             />
+            <AboutUs/>
             <Footer/>
         
         
