a187c19d371c91896a2106834d086adf8fd56bba
Arnold <dioufarnold02@gmail.com>
2024-02-18 17:07:03 +0300

    Added AboutUs Component

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,16 @@
+import "./AboutUsStyles.css";
+
+function AboutUs() {
+    return(
+        <div className="about-container">
+            <h1>Our History</h1>
+            <p>Travel is a journey of discovery. At Travel, we believe travel is a force for good. It's a way to make the world a better place, to connect people and places, to create mutual understanding, and to build a more sustainable world.</p>
+            <h1>Our Mission</h1>
+            <p>Our mission is to make travel better for everyone. We believe that travel should be a force for good. It's a way to make the world a better place, to connect people and places, to create mutual understanding, and to build a more sustainable world.</p>
+            <h1>Our Vision</h1>
+            <p>Our vision is to make travel better for everyone. We believe that travel should be a force for good. It's a way to make the world a better place, to connect people and places, to create mutual understanding, and to build a more sustainable world.</p>
+        </div>
+    )
+}
+
+export default AboutUs;
